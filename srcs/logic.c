18a7b4ba737610f7ac46d248c539b3bf3b109540
Niklas Weber <nweber@student.42heilbronn.de>
2025-07-15 18:11:27 +0200

    first util functions and shortend length variable

--- srcs/logic.c
+++ srcs/logic.c
@@ -3,22 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   logic.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nweber <nweber@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*   By: nweber <nweber@student.42Heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/14 20:34:13 by nweber            #+#    #+#             */
-/*   Updated: 2025/07/14 20:34:13 by nweber           ###   ########.fr       */
+/*   Updated: 2025/07/15 16:25:13 by nweber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	sort1(t_stack *stack_a, t_stack *stack_b, int length)
+void	sort1(t_stack *stack_a, t_stack *stack_b, int len)
 {
 	int	i;
 	int	range;
 
 	i = 0;
-	range = ft_sqrt(length) * 14 / 10;
+	range = ft_sqrt(len) * 14 / 10;
 	while (stack_a->head)
 	{
 		if (stack_a->head->s_index <= i)
@@ -37,28 +37,28 @@ void	sort1(t_stack *stack_a, t_stack *stack_b, int length)
 	}
 }
 
-void	sort2(t_stack *stack_a, t_stack *stack_b, int length)
+void	sort2(t_stack *stack_a, t_stack *stack_b, int len)
 {
 	int	rb_count;
 	int	rrb_count;
 
-	while (length - 1 >= 0)
+	while (len - 1 >= 0)
 	{
-		rb_count = count_r(stack_b->head, length - 1);
-		rrb_count = (length + 3) - rb_count;
+		rb_count = count_r(stack_b->head, len - 1);
+		rrb_count = (len + 3) - rb_count;
 		if (rb_count <= rrb_count)
 		{
-			while (stack_b->head->s_index != length - 1)
+			while (stack_b->head->s_index != len - 1)
 				rb(stack_b, 'b', true);
 			pa(stack_a, stack_b, 'a', true);
-			length--;
+			len--;
 		}
 		else
 		{
-			while (stack_b->head->s_index != length - 1)
+			while (stack_b->head->s_index != len - 1)
 				rrb(stack_b, 'b', true);
 			pa(stack_a, stack_b, 'a', true);
-			length--;
+			len--;
 		}
 	}
 }
--- srcs/utils.c
+++ srcs/utils.c
@@ -3,12 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nweber <nweber@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*   By: nweber <nweber@student.42Heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/14 20:41:23 by nweber            #+#    #+#             */
-/*   Updated: 2025/07/14 20:41:23 by nweber           ###   ########.fr       */
+/*   Updated: 2025/07/15 16:41:08 by nweber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	check_sort(t_stack *stack)
+{
+	t_node	*temp;
+
+	temp = stack->head;
+	while (temp->next)
+	{
+		if (temp->value > temp->next->value)
+			return (0);
+		temp = temp->next;
+	}
+	return (1);
+}
+
+void	free_stack(t_stack *stack)
+{
+
+}
 
