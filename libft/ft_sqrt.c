b925d031257b5c322ac1a3e5bbe77addd2b55de4
nweber23 <nweber@student.42heilbronn.de>
2025-08-11 13:02:58 +0200

    fixed sqrt

--- libft/ft_sqrt.c
+++ libft/ft_sqrt.c
@@ -12,20 +12,19 @@
 
 #include "libft.h"
 
-int	ft_sqrt(int n)
+int	ft_sqrt(int number)
 {
 	int	i;
 
-	if (n < 0)
-		return (-1);
-	if (n == 0 || n == 1)
-		return (n);
-	i = 1;
-	while (i * i <= n)
-	{
-		if (i * i == n)
-			return (i);
+	if (number < 4)
+		return (1);
+	i = 2;
+	while (i * i < number)
 		i++;
+	if (i * i > number)
+	{
+		if ((i * i - number) < ((i - 1) * (i - 1) + (-number)))
+			return (i);
 	}
-	return (-1);
+	return (i - 1);
 }
